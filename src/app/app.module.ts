ac3320c6a74e9b150ebc3a2253d8d5904ecb4579
GitdoPedro <emailpedrolima@gmail.com>
2023-01-19 16:43:24 -0300

    [M1S11] Ex 7 - ngIf e else

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { CapitalizePhilipsPipe } from './pipes/capitalize-philips.pipe';
 import { Ex4Component } from './ex4/ex4.component';
 import { Ex6Component } from './ex6/ex6.component';
+import { Ex7Component } from './ex7/ex7.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CapitalizePhilipsPipe,
     Ex4Component,
-    Ex6Component
+    Ex6Component,
+    Ex7Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ex7/ex7.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex7',
+  templateUrl: './ex7.component.html',
+  styleUrls: ['./ex7.component.scss']
+})
+export class Ex7Component {
+  nota: any = 0
+
+}
