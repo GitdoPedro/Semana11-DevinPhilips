2349c8b8589448bef991664d732b91cdd84b81e8
GitdoPedro <emailpedrolima@gmail.com>
2023-01-19 16:00:49 -0300

    [M1S11] Ex 6 - ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CapitalizePhilipsPipe } from './pipes/capitalize-philips.pipe';
 import { Ex4Component } from './ex4/ex4.component';
+import { Ex6Component } from './ex6/ex6.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CapitalizePhilipsPipe,
-    Ex4Component
+    Ex4Component,
+    Ex6Component
   ],
   imports: [
     BrowserModule
